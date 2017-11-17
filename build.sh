84af4a344ffd08a19c538de46ec2348e94ec6b6e
Dmytro Titov <dmytro.titov@gmail.com>
2017-11-17 12:44:06 +0100

    Refactor Docker scripts.

--- build.sh
+++ build.sh
@@ -2,13 +2,9 @@
 
 set -e
 
-docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-
 for d in cwl/*/ ; do
     IMAGE=${d/cwl/conglomerate}
     IMAGE=${IMAGE%?};
     TOOL=${IMAGE/conglomerate}
-    docker pull ${IMAGE}:latest
     docker build --cache-from ${IMAGE}:latest --tag ${IMAGE}:latest cwl${TOOL}
-    docker push ${IMAGE}:latest
 done
---
+++ pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+for d in cwl/*/ ; do
+    IMAGE=${d/cwl/conglomerate}
+    IMAGE=${IMAGE%?};
+    docker pull ${IMAGE}:latest
+done
