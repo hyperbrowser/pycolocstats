c8a07504a2639043037765632e0a610b27ccda6e
Dmytro Titov <dmytro.titov@gmail.com>
2017-11-22 11:07:29 +0100

    Add GoShifter, Giggle and LOLA draft classes.

---
+++ conglomerate/methods/lola/lola.py
@@ -0,0 +1,32 @@
+from conglomerate.methods.method import Method
+from conglomerate.tools.constants import LOLA_TOOL_NAME
+
+
+class LOLA(Method):
+    def _getToolName(self):
+        return LOLA_TOOL_NAME
+
+    def _setDefaultParamValues(self):
+        pass
+
+    def setTrackFileNames(self, trackFnList):
+        assert len(trackFnList) == 2
+        pass
+
+    def setChromLenFileName(self, chromLenFileName):
+        pass
+
+    def setAllowOverlaps(self, allowOverlaps):
+        assert allowOverlaps is True
+
+    def _parseResultFiles(self):
+        pass
+
+    def getPValue(self):
+        pass
+
+    def getTestStatistic(self):
+        pass
+
+    def getFullResults(self):
+        pass
--- conglomerate/tools/constants.py
+++ conglomerate/tools/constants.py
@@ -1,3 +1,6 @@
 GENOMETRICORR_TOOL_NAME = 'genometricorr'
 STEREOGENE_TOOL_NAME = 'stereogene'
 INTERVALSTATS_TOOL_NAME = 'intervalstats'
+GOSHIFTER_TOOL_NAME = 'goshifter'
+GIGGLE_TOOL_NAME = 'giggle'
+LOLA_TOOL_NAME = 'lola'
