ce6e23cab26ef454fb953701b67fd8fe6fb1caec
Dmytro Titov <dmytro.titov@gmail.com>
2017-11-28 15:51:52 +0100

    Fix temp files removal in runner.

--- conglomerate/tools/runner.py
+++ conglomerate/tools/runner.py
@@ -3,17 +3,18 @@ from conglomerate.tools.util import deleteAllTmpFiles
 
 
 def runAllMethodsInSequence(methods):
-    for method in methods:
-        jobs = method.createJobs()
-        if len(jobs) == 1:
-            job = jobs[0]
-            resultFilesDict = job.run()
-            method.setResultFilesDict(resultFilesDict)
-        elif len(jobs) > 1:
-            assert isinstance(method, MultiMethod)
-            resultFilesDictList = [job.run() for job in jobs]
-            method.setResultFilesDictList(resultFilesDictList)
-        else:
-            raise NotImplementedError()
-
-    deleteAllTmpFiles()
+    try:
+        for method in methods:
+            jobs = method.createJobs()
+            if len(jobs) == 1:
+                job = jobs[0]
+                resultFilesDict = job.run()
+                method.setResultFilesDict(resultFilesDict)
+            elif len(jobs) > 1:
+                assert isinstance(method, MultiMethod)
+                resultFilesDictList = [job.run() for job in jobs]
+                method.setResultFilesDictList(resultFilesDictList)
+            else:
+                raise NotImplementedError()
+    finally:
+        deleteAllTmpFiles()
