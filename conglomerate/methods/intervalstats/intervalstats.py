c716c4ee8ce29445253e6d776db1a6f1f90905cc
dianadom <dianadom@ifi.uio.no>
2017-11-30 10:58:46 +0100

    IntervalStats

--- conglomerate/methods/intervalstats/intervalstats.py
+++ conglomerate/methods/intervalstats/intervalstats.py
@@ -1,5 +1,6 @@
 from conglomerate.methods.method import OneVsOneMethod
 from conglomerate.tools.constants import INTERVALSTATS_TOOL_NAME
+from conglomerate.tools.util import getTemporaryFileName
 
 
 class IntervalStats(OneVsOneMethod):
@@ -16,7 +17,19 @@ class IntervalStats(OneVsOneMethod):
         self._params['r'] = trackFn
 
     def setChromLenFileName(self, chromLenFileName):
-        self._params['d'] = chromLenFileName
+        contents = []
+        with open(chromLenFileName, 'r') as f:
+            for line in f.readlines():
+                newl = line.strip('\n').split('\t')
+                contents.append([newl[0], '0', newl[1]])
+
+        tempFileName = getTemporaryFileName()
+        sampleFile = open(tempFileName, 'w')
+        for c in contents:
+            sampleFile.write('\t'.join(c)+'\n')
+        sampleFile.flush()
+
+        self._params['d'] = sampleFile.name #chromLenFileName
         # TODO: Add an extra column in between (filled with zeroes), e.g.:
         # chr1	0	249250621
         # chr10	0	135534747
@@ -28,19 +41,35 @@ class IntervalStats(OneVsOneMethod):
         assert allowOverlaps is True
 
     def _parseResultFiles(self):
-        pass
+        #links to output files
+        resultsFolderPath = self._resultFilesDict['output'] + 'output'
+
+        self._pvals = {}
+        self._pvals[(self._params['q'], self._params['r'])] = {}
+
+        self._testStats = {}
+        self._testStats[(self._params['q'], self._params['r'])] = {}
+
+        with open(resultsFolderPath, 'r') as f:
+            for line in f.readlines():
+                newLine = line.strip('\n').split('\t')
+                self._pvals[(self._params['q'], self._params['r'])][newLine[0]] = newLine[6]
+                self._testStats[(self._params['q'], self._params['r'])][newLine[0]] = newLine[3]
+
 
     def getPValue(self):
-        pass
+        return self._pvals
 
     def getTestStatistic(self):
-        pass
+        return self._testStats
 
     def getFullResults(self):
-        pass
+        resultsFolderPath = self._resultFilesDict['output'] + 'output'
+        return open(resultsFolderPath).read()
 
     def preserveClumping(self, preserve):
-        pass
+        #not sure yet
+        assert preserve is False
 
     def setRestrictedAnalysisUniverse(self, restrictedAnalysisUniverse):
         pass
