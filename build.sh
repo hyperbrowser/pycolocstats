6f6d49846a23607d1420838f3fc2165e36ba024e
Dmytro Titov <dmytro.titov@gmail.com>
2017-12-06 16:21:31 +0100

    Remove travis_wait and quite options.

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ for d in cwl/*/ ; do
     IMAGE=${d/cwl/conglomerate}
     IMAGE=${IMAGE%?};
     TOOL=${IMAGE/conglomerate}
-    docker build --quiet --cache-from ${IMAGE}:latest --tag ${IMAGE}:latest cwl${TOOL}
+    docker build --cache-from ${IMAGE}:latest --tag ${IMAGE}:latest cwl${TOOL}
 done
