db044d1bef4529b2deb6d49608fc3c069fde3ede
dianadom <dianadom@ifi.uio.no>
2017-12-11 11:23:01 +0100

    goshifter

--- conglomerate/methods/goshifter/goshifter.py
+++ conglomerate/methods/goshifter/goshifter.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from os import path, listdir
+
 from conglomerate.methods.method import OneVsOneMethod
 from conglomerate.tools.constants import GOSHIFTER_TOOL_NAME
 
@@ -29,16 +31,31 @@ class GoShifter(OneVsOneMethod):
         assert allowOverlaps is True
 
     def _parseResultFiles(self):
-        pass
+        textOut = self.getResultFilesDict()['stdout']
+
+        self._pvals={}
+
+        #get p-value from stdout output
+        pValText = 'p-value = '
+        valStart = textOut.find(pValText)
+        valEnd = textOut.find('Detailed')
+        self._pvals[(self._params['a'], self._params['o'])] = textOut[valStart + len(pValText):valEnd]
+        self._testStats[(self._params['a'], self._params['o'])] = -1
+
+        for fi in listdir(path.join(self._resultFilesDict['output'])):
+            if 'nperm10.enrich' in fi:
+                with open(path.join(self._resultFilesDict['output'], fi), 'r') as f:
+                    self._testStats[(self._params['a'], self._params['o'])] = f.readlines()[1].split('\t')[1]
+
 
     def getPValue(self):
-        pass
+        return self._pvals
 
     def getTestStatistic(self):
-        pass
+        return self._testStats
 
     def getFullResults(self):
-        pass
+        return open(self.getResultFilesDict()['stdout']).read()
 
     def preserveClumping(self, preserve):
         pass
