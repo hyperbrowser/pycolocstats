16a3761d77f0cd959d9c6221e6c13dddfcb691da
Geir Kjetil Sandve <geirksa@ifi.uio.no>
2017-12-28 23:24:59 +0100

    Small fixes

--- conglomerate/tools/ToolResultsCacher.py
+++ conglomerate/tools/ToolResultsCacher.py
@@ -1,5 +1,10 @@
+from conglomerate.tools.constants import VERBOSE_RUNNING
+
 STORE_IN_CACHE = True
 LOAD_FROM_CACHE = True
+import pickle
+import os
+
 
 class ToolResultsCacher(object):
     #CACHE_DISK_PATH = '/Users/sandve/egne_dokumenter/_faglig/conglomerateColoc/cache/'
@@ -7,27 +12,29 @@ class ToolResultsCacher(object):
     def __init__(self, tool, params):
         self._toolName = tool._toolName
         self._params = params
-        self._cacheKey = str(hash((self._toolName, tuple(sorted(self._params.items())))))
-        #self._cacheKey = str(hash((self._toolName, tuple(sorted([x for x in self._params.items() if not x[1].startswith('/tmp')] )))))
+        try:
+            self._cacheKey = str(hash((self._toolName, tuple(sorted(self._params.items())))))
+            self._cacheFn = self.CACHE_DISK_PATH + self._cacheKey
+        except:
+            self._cacheKey = None
+            self._cacheFn = None
+            #self._cacheKey = str(hash((self._toolName, tuple(sorted([x for x in self._params.items() if not x[1].startswith('/tmp')] )))))
         #print('TEMP2: ', tuple(sorted(self._params.items())))
-        self._cacheFn = self.CACHE_DISK_PATH + self._cacheKey
 
     def store(self, toolResults):
         if STORE_IN_CACHE:
-            #print('TEMP1: ', self._cacheKey)
-            import pickle
             pickle.dump(toolResults, open(self._cacheFn,'w'))
 
     def load(self):
         if self.cacheAvailable():
-            import pickle
+            if VERBOSE_RUNNING:
+                print 'Loading cached results for: ', self._toolName
             return pickle.load(open(self._cacheFn))
         else:
             return None
 
     def cacheAvailable(self):
-        if LOAD_FROM_CACHE:
-            import os
+        if LOAD_FROM_CACHE and self._cacheFn is not None:
             return os.path.exists(self._cacheFn)
         else:
             return False
