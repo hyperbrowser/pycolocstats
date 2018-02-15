3c1bf331ac4d46f92c2bc05210c1257aeb7da121
Sveinung Gundersen <sveinugu@gmail.com>
2018-02-15 15:51:12 +0100

    Added tmp and job output dir config options. Added updating of config file from sample for new settings

--- conglomerate/core/config.py
+++ conglomerate/core/config.py
@@ -7,6 +7,14 @@ CONFIG_SECTION = u'Conglomerate'
 DEFAULT_CONFIG_REL_FN = '../config/conglomerate.ini'
 
 
+def getConfigParser(config_fn):
+    from configparser import ConfigParser
+    cfgParser = ConfigParser()
+    cfgParser.read(config_fn)
+    assert len(cfgParser.sections()) == 1 and cfgParser.sections()[0] == CONFIG_SECTION
+    return cfgParser
+
+
 def getConfigFilename():
     import os
     import pkg_resources
@@ -17,21 +25,23 @@ def getConfigFilename():
         return envConfigFn
     else:
         defConfigFn = pkg_resources.resource_filename('conglomerate', DEFAULT_CONFIG_REL_FN)
+        sampleConfigFn = defConfigFn + '.sample'
+
         if not os.path.exists(defConfigFn):
-            sampleConfigFn = defConfigFn + '.sample'
             shutil.copy(sampleConfigFn, defConfigFn)
+        else:
+            sampleCfgParser = getConfigParser(sampleConfigFn)
+            sampleCfgParser.read(defConfigFn)
+
+            with open(defConfigFn, 'w') as defConfigFile:
+                sampleCfgParser.write(defConfigFile)
+
         return defConfigFn
-
-
-def getConfigParser(config_fn):
-    from configparser import ConfigParser
-    cfgParser = ConfigParser()
-    cfgParser.read(config_fn)
-    assert len(cfgParser.sections()) == 1 and cfgParser.sections()[0] == CONFIG_SECTION
-    return cfgParser
 
 
 cfgParser = getConfigParser(getConfigFilename())
 
 CATCH_METHOD_EXCEPTIONS = cfgParser.getboolean(CONFIG_SECTION, 'catch_method_exceptions')
-VERBOSE_RUNNING = cfgParser.getboolean(CONFIG_SECTION, 'verbose_running')
+VERBOSE_RUNNING = cfgParser.get(CONFIG_SECTION, 'verbose_running')
+TMP_DIR = cfgParser.get(CONFIG_SECTION, 'tmp_dir')
+DEFAULT_JOB_OUTPUT_DIR = cfgParser.get(CONFIG_SECTION, 'default_job_output_dir')
