6af8bb763d34aa1d3a0aee9ba121532ce4b033d0
Boris Simovski <boris.simovski@gmail.com>
2018-02-19 14:33:58 +0100

    Add stereogene

--- conglomerate/tools/WorkingMethodObjectParser.py
+++ conglomerate/tools/WorkingMethodObjectParser.py
@@ -1,10 +1,11 @@
 from conglomerate.core.config import VERBOSE_RUNNING
+from conglomerate.methods.stereogene.stereogene import StereoGene
 from conglomerate.tools.method_compatibility import getCompatibleMethodObjects
 from conglomerate.methods.genometricorr.genometricorr import GenometriCorr
 from conglomerate.methods.giggle.giggle import Giggle
 from conglomerate.methods.intervalstats.intervalstats import IntervalStats
 from conglomerate.methods.lola.lola import LOLA
-ALL_CONGLOMERATE_METHOD_CLASSES = [GenometriCorr, Giggle, IntervalStats, LOLA]
+ALL_CONGLOMERATE_METHOD_CLASSES = [GenometriCorr, Giggle, IntervalStats, LOLA, StereoGene]
 
 class WorkingMethodObjectParser:
 
