24ee2db317df9dd34ae9cf6ebeb407e23385e900
Sveinung Gundersen <sveinugu@gmail.com>
2018-03-01 12:25:17 +0100

    Test images: Fixes pull.sh logic

--- pull.sh
+++ pull.sh
@@ -7,23 +7,17 @@ USE_TEST_DOCKER_IMAGES=$(/usr/bin/env python -c "from __future__ import print_fu
 
 if [[ "$USE_TEST_DOCKER_IMAGES" == "True" ]]; then
     TEST_IMAGES=cwl/*_test/
-    echo $TEST_IMAGES
-
-    for d in $TEST_IMAGES ; do
-        IMAGE=${d/cwl/conglomerate}
-        IMAGE=${IMAGE%?};
-        echo Running: docker pull ${IMAGE}:latest
-        docker pull ${IMAGE}:latest
-    done
 else
     TEST_IMAGES=''
 fi
 
 for d in cwl/*/ ; do
-    if echo ${TEST_IMAGES[@]} | grep -q -w -v "$d"; then
-        IMAGE=${d/cwl/conglomerate}
-        IMAGE=${IMAGE%?};
-        echo Running: docker pull ${IMAGE}:latest
-        docker pull ${IMAGE}:latest
+    if echo ${TEST_IMAGES[@]} | grep -q -w -v ${d%?}_test/; then
+        if [[ $d != *_test/ || "$USE_TEST_DOCKER_IMAGES" == "True" ]]; then
+            IMAGE=${d/cwl/conglomerate}
+            IMAGE=${IMAGE%?};
+            echo Running: docker pull ${IMAGE}:latest
+            docker pull ${IMAGE}:latest
+        fi
     fi
 done
