02b0acfe983ede76d67804508a36aa4f2d1f1f7d
Boris Simovski <boris.simovski@gmail.com>
2018-05-04 16:13:57 +0200

    bugfix

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ for d in src/cwl/*/ ; do
     IMAGE=${d/src\/cwl/colocstats}
     IMAGE=${IMAGE%?};
     TOOL=${IMAGE/colocstats}
-    echo Running: docker build --cache-from ${IMAGE}:latest --tag ${IMAGE}:latest cwl${TOOL}
+    echo Running: docker build --cache-from ${IMAGE}:latest --tag ${IMAGE}:latest src/cwl${TOOL}
     docker build --cache-from ${IMAGE}:latest --tag ${IMAGE}:latest cwl${TOOL}
 done
